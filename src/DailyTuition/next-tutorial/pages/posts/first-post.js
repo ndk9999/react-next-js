2a1a32e770dd876bb273a6a643b35860dc19e0d5
Phuc Nguyen <phuc.nguyen@devsoft.vn>
2022-06-28 15:14:12 +0700

    Create a Next app and add new page

---
+++ src/DailyTuition/next-tutorial/pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function FirstPost() {
+    return (
+        <>
+            <Head>
+                <title>Next.js First Post</title>
+            </Head>
+            <h1>First Blog Post</h1>
+        </>
+    )
+}
