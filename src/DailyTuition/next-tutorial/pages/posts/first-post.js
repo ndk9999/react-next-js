ac687dbe0baec9eca27858cb8d10ff05ab470a00
Phuc Nguyen <phuc.nguyen@devsoft.vn>
2022-06-28 15:44:07 +0700

    Add assets and CSS files

--- src/DailyTuition/next-tutorial/pages/posts/first-post.js
+++ src/DailyTuition/next-tutorial/pages/posts/first-post.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import nextjs from '../../public/images/nextjs.png'
+import styles from '../../styles/sass/style.module.scss'
 
 export default function FirstPost() {
     return (
@@ -6,7 +9,25 @@ export default function FirstPost() {
             <Head>
                 <title>Next.js First Post</title>
             </Head>
-            <h1>First Blog Post</h1>
+
+            <h1 className={styles.title}>First Blog Post</h1>
+            <h2 className='title'>Post Title</h2>
+            <style jsx>
+            {
+                `
+                .title {
+                    font-size: 60px;
+                    color: red;
+                }
+                `
+            }
+            </style>
+
+            <Image 
+                src='/images/nextjs.png'
+                width={300}
+                height={300}
+            />
         </>
     )
 }
