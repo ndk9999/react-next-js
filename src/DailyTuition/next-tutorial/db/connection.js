9ed5b28496a2029b463552bbeb24304c0c42191b
Phuc Nguyen <phuc.nguyen@devsoft.vn>
2022-08-25 16:09:40 +0700

    Update connection string

--- src/DailyTuition/next-tutorial/db/connection.js
+++ src/DailyTuition/next-tutorial/db/connection.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 
 const connectMongoDb = async () => {
-    await mongoose.connect('mongodb+srv://ndk9999:Ph0ng&5On@cluster0.xfhls.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://ndk9999:<password>@cluster0.xfhls.mongodb.net/?retryWrites=true&w=majority');
     console.log('Database connected');
 }
 
